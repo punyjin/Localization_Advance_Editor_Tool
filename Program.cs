14c8a89d8fb01c8b098d1d93abac4f98512dd7ed
xorbit <91112547+punyjin@users.noreply.github.com>
2024-06-10 20:58:54 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UndeadLegacy_xorbit
+namespace Localization_Editor
 {
     internal static class Program
     {
